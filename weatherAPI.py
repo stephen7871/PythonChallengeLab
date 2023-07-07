039dc1d1d82da56b6f5e85419c421bc6d8d63a14
arid <arid@g.com>
2023-07-07 10:53:19 -0400

    Added code to make api all

--- weatherAPI.py
+++ weatherAPI.py
@@ -1,1 +1,18 @@
-print("hello word")
+"""
+- Team 2 
+- weatherAPI.py
+- Used to make api requests 
+
+"""
+
+import requests
+
+URL = "http://api.openweathermap.org/data/2.5/weather"
+city = input("Enter the city: ")
+
+API_KEY = '51feb6438334e6502cf871203e59a9af'
+
+body = {'appid': API_KEY, 'q' : city}
+response = requests.get(URL, params = body)
+
+print(response.json())
